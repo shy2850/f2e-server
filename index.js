76f7bddee54b994dbce323595cfe126782a8b0a2
shiyangyang <shiyangyang@flowpp.com>
2017-11-02 09:34:01 +0800

    v2.6.3 暴露中间件

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-module.exports = require('./lib/server/index')
+const server = require('./lib/server/index')
+const middlewares = {
+    babel: require('./lib/middleware/babel'),
+    include: require('./lib/middleware/include'),
+    less: require('./lib/middleware/less'),
+    livereload: require('./lib/middleware/ServerSent')
+}
+
+Object.assign(server, {
+    middlewares
+})
+
+module.exports = server
