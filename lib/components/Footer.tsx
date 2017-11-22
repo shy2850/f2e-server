2f0d006845bd3eda09e01f1592b7cc500bb51766
shy2850 <shy2850.com>
2017-11-22 23:05:03 +0800

    install

--- lib/components/Footer.tsx
+++ lib/components/Footer.tsx
@@ -4,7 +4,7 @@ export default () => <footer className="footer">
     <div className="container">
         <div className="content has-text-centered">
             <p>
-                <strong>f2e-blog</strong> by <a href="https://github.com/shy2850">shy2850</a>. The source code is licensed
+                <a href="https://github.com/shy2850/f2e-blog"><strong>f2e-blog</strong></a> by <a href="https://github.com/shy2850">shy2850</a>. The source code is licensed
                 <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
             </p>
         </div>
---
+++ src/install.md
@@ -0,0 +1,17 @@
+# 安装
+
+f2e-server 支持全局安装和局部安装使用
+title: f2e-server 安装
+keywords: 安装
+description: f2e-server 支持全局安装和局部安装使用
+date: 2017/11/22
+
+## 环境准备
+
+> Node.js >= 8.9.1
+
+## 全局安装
+
+``` shell
+npm i -g f2e-server
+```
