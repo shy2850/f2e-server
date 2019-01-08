ffdc663aaf6e854099a2f34f5727b0dc861aabfa
shiyangyang <shiyangyang@flowpp.com>
2019-01-08 20:08:46 +0800

    f2e-serve@0.3.4

--- serve/out/ServerSent.js
+++ serve/out/ServerSent.js
@@ -7,26 +7,31 @@ module.exports = (fn, conf = {
         'Connection': 'keep-alive'
     })
 
+    let interval1
+    let interval2
+
     const heart_beat = function heart_beat() {
         if (resp.writable && !resp.finished) {
             resp.write(`data:1\n\n`)
-            setTimeout(heart_beat, 100000)
+            interval1 = setTimeout(heart_beat, 100000)
         }
     }
 
     const loop = function loop () {
         const res = fn(req, resp, conf)
-        if (res) {
+        if (res && res.writable && !resp.finished) {
             resp.write(`data:${JSON.stringify(res)}\n\n`)
         }
         if (conf.interval) {
-            setTimeout(loop, conf.interval)
+            interval2 = setTimeout(loop, conf.interval)
         } else {
             heart_beat()
         }
     }
 
     req.connection.addListener('close', () => {
+        clearTimeout(interval1)
+        clearTimeout(interval2)
         resp.end()
     }, false)
     loop()
