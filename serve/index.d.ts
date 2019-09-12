c1a9413ea2b48c1d53e8d60a8230586b04cfc5c3
shiyangyang <shiyangyang@flowpp.com>
2019-09-12 17:57:56 +0800

    v0.4.3

--- serve/index.d.ts
+++ serve/index.d.ts
@@ -3,7 +3,7 @@ import { F2EConfig } from 'f2e-server'
 import { MemoryTree } from "memory-tree"
 
 export interface ExecFn {
-    (req: IncomingMessage, resp: ServerResponse): undefined | false
+    (req: IncomingMessage, resp: ServerResponse): any
 }
 export interface Callback<T extends Object = {}> {
     (req?: IncomingMessage, resp?: ServerResponse, conf?: F2EConfig): T | Promise<T>
@@ -25,7 +25,7 @@ export interface Out {
 
 export class Route {
     execute: {
-        (pathname: string, req: IncomingMessage, res: ServerResponse, memory: MemoryTree.Store): undefined | false
+        (pathname: string, req: IncomingMessage, res: ServerResponse, memory: MemoryTree.Store): any
     }
     on: {
         (reg: string | RegExp, exec: ExecFn): void
